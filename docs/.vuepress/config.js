398df59f44d920be5ec78f85926b6ec08b367740
zhouzhikai3 <zhouzhikai3@jd.com>
2022-10-17 23:25:27 +0800

    feat: add search plugin

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,28 @@
+import { defaultTheme } from 'vuepress'
+import { searchPlugin } from '@vuepress/plugin-search'
+
+export default {
+    title: 'Jace Chou\'s Blog',
+    theme: defaultTheme({
+        navbar: [
+            { text: '首页', link: '/' },
+            {
+                text: '库',
+                link: '/repository'
+            },
+            {
+                text: '网站',
+                link: '/website'
+            },
+            {
+                text: '文章',
+                link: '/article'
+            },
+        ]
+    }),
+    plugins: [
+        searchPlugin({
+            // options
+        })
+    ]
+}
