1b2c27d8c0d02367c70b60582eb776b438d652b4
MrZhouZh <17600330793@sina.cn>
2023-03-12 21:34:59 +0800

    feat: update Observer

--- docs/_code/Design-pattern/Emitter.js
+++ docs/_code/Design-pattern/Emitter.js
@@ -1,3 +1,6 @@
+/**
+ * 发布订阅
+ */
 class Emitter {
   constructor() {
     this.emits = {}
@@ -10,6 +13,7 @@ class Emitter {
 
     if (this.emits[emitName].includes(callback)) {
       // 事件重复注册
+      console.log(`${emitName} 事件重复注册!`)
     } else {
       this.emits[emitName].push({
         handler: callback
@@ -30,6 +34,7 @@ class Emitter {
 
   emit(emitName, ...args) {
     if (!this.emits[emitName]) return
+
     this.emits[emitName].forEach((item, index) => {
       item.handler(...args)
 
@@ -41,11 +46,34 @@ class Emitter {
 
   off(emitName, callback) {
     if (this.emits[emitName]) {
-      if (callback && callback instanceof Function && this.emits[emitName].includes(callback)) {
-        this.emits[emitName] = this.emits[emitName].filter((v) => v.handler !== callback)
+      if (
+        callback &&
+        callback instanceof Function &&
+        this.emits[emitName].includes(callback)
+      ) {
+        this.emits[emitName] = this.emits[emitName].filter(v => v.handler !== callback)
       } else {
         delete this.emits[emitName]
       }
     }
   }
 }
+
+// ---------- Test ---------
+const emitter = new Emitter()
+
+function user1(content) {
+  console.log('user1 on', content)
+}
+
+function user2(content) {
+  console.log('user2 on', content)
+}
+emitter.on('test', testHandle)
+emitter.on('test2', test2Handle)
+
+emitter.emit('test', 'emit...') // user1 on emit...
+emitter.emit('test2', 'emit2...') // user2 on emit2...
+
+emitter.off('test', testHandle)
+emitter.emit('test', testHandle)  // no output
--- docs/_post/Design-pattern/Observer.md
+++ docs/_post/Design-pattern/Observer.md
@@ -8,7 +8,23 @@ category: Design-pattern
 
 允许你制定一种订阅机制, 可在对象事件发生时通知多个 "观察" 该对象的其他对象
 
-> 详看: [https://mrzhouzh.github.io/learning-code/Design-pattern/Observer/README.html](https://mrzhouzh.github.io/learning-code/Design-pattern/Observer/README.html)
+### 优点
+
+- **开闭原则** 无需修改发布者代码就能引入新的订阅者类
+
+- 可在运行时建立对象之间的联系
+
+### 缺点
+
+- 订阅者的通知顺序是随机的
+
+### 适用场景
+
+- 当一个对象状态的改变需要改变其他对象, 或实际对象是事先未知的或动态变化的时
+
+  例如你创建了自定义按钮类并允许客户端在按钮中注入自定义代码, 这样当用户按下按钮时就会触发这些代码
+
+- 当应用中的一些对象必须观察其他对象时, 可使用该模式, 但仅能在有限时间内或特定的情况下使用
 
 ### 发布订阅实现
 
@@ -18,4 +34,6 @@ category: Design-pattern
 
 :::
 
+### 常见问题
 
+[观察者 VS 发布订阅](https://hackernoon.com/observer-vs-pub-sub-pattern-50d3b27f838c)
