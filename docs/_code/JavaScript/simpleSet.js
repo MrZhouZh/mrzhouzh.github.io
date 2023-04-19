59b8c188c5982f96dc4c262e1e17ba7b868c39eb
MrZhouZh <17600330793@sina.cn>
2023-04-19 22:42:18 +0800

    chore: simple set ready to do

---
+++ docs/_code/JavaScript/simpleSet.js
@@ -0,0 +1,23 @@
+// const set = new Set()
+// set.add(1)
+// set.delete()
+// set.has()
+// set.clear()
+// set.size()
+
+
+class SimpleSet {
+  constructor() {
+    this.item = {}
+  }
+
+  add(value) {}
+
+  delete(value) {}
+
+  has(value) {}
+
+  clear() {}
+  
+  size() {}
+}
