5eabde29750aa8c47ca3fa46033a1ccf11f13757
MrZhouZh <17600330793@sina.cn>
2023-05-31 12:08:10 +0800

    feat: add stringify

---
+++ docs/_code/Interview/experience-notes.js
@@ -0,0 +1,46 @@
+/* 面经手写题抄录 */
+/**
+ * 将数组转为字符串
+ * @param {array} list 
+ * @returns {string}
+ * 
+ * @example
+ * ```
+ * const test = () => {
+ *  const list = [
+ *    {
+ *      input: [1, 2, 3, [4, 5]],
+ *      output: '[ 1 2 3 [ 4 5 ] ]'
+ *    },
+ *    {
+ *      input: [79, [[88, 98], 99]],
+ *      output: '[ 79 [ [ 88 98 ] 99 ] ]'
+ *    },
+ *  ]
+ * 
+ *  return list.map(item => {
+ *    const res = stringify(item.input)
+ *    return res === item.output
+ *  })
+ * }
+ * // [true, true]
+ * ```
+ */
+function stringify(list) {
+  const _stringify = (list, res = '') => {
+    res += '[ '
+    for (const item of list) {
+      if (Array.isArray(item)) {
+        res += _stringify(item)
+      } else {
+        res += item
+      }
+    }
+    res += '] '
+    return res
+  }
+
+  return _stringify(list).replace(/\s$/, '')
+}
+
+
